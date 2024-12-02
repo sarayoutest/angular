6756c2143413c88072f2b577ac5f87de5f03d267
sara youssef <154534692+sarayoutest@users.noreply.github.com>
2024-12-02 23:48:04 +0300

    Add files via upload

---
+++ server.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = 'https://reqres.in/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(page: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}?page=${page}`);
+  }
+
+  getUserDetails(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+}
